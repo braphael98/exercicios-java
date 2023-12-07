49c2c2bb1d4d4e0f8181d1e6d80f1fef4fb2b545
Bruno <braphael98@gmail.com>
2023-12-07 20:52:52 -0300

    Cachorro funcionou

--- Aula02.java
+++ Aula02.java
@@ -2,12 +2,22 @@ public class Aula02 {
     public static void main(String[] args) {
         Pernilongo jorge = new Pernilongo();
         jorge.atrapalharSono();
+        Cachorro getulio = new Cachorro();
+        getulio.atrapalharSono();
 
     }
 }
 
 class Pernilongo {
     String barulho = "piiiii";
+
+    public void atrapalharSono() {
+        System.out.println(this.barulho);
+    }
+}
+
+class Cachorro {
+    String barulho = "Au au";
 
     public void atrapalharSono() {
         System.out.println(this.barulho);
