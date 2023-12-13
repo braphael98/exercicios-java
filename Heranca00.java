db1e1d5a055ff29c1e11718c1f8d69093350662b
Bruno <braphael98@gmail.com>
2023-12-12 21:37:44 -0300

    descricao dos animais

--- Heranca00.java
+++ Heranca00.java
@@ -3,8 +3,15 @@ public class Heranca00 {
         System.out.println("funfeia!");
         Cachorro fred = new Cachorro();
         fred.fazBarulho();
+        fred.descrever();
+
         Gato minerin = new Gato();
         minerin.fazBarulho();
+        minerin.descrever();
+
+        Javali jorge = new Javali();
+        jorge.fazBarulho();
+        jorge.descrever();
 
     }
 
@@ -20,18 +27,41 @@ abstract class Animal {
         System.out.println(this.onomatopeia);
     }
 
+    public void descrever() {
+        System.out.println("Raça: " + this.raca);
+        System.out.println("Idade: " + this.idade);
+        System.out.println("Cor: " + this.cor);
+
+    }
+
 }
 
 class Cachorro extends Animal {
-    Cachorro() {
+    Cachorro() { // metodo construtor, o que define o cão !
         this.onomatopeia = "au au";
-        
+        this.raca = "vira-lata";
+        this.cor = "Caramelo";
+        this.idade = 2;
     }
 }
 
 class Gato extends Animal {
-    public Gato() {
+    public Gato() { //metodo construtor, que define o gato !
         this.onomatopeia = "miar";
+        this.raca = "de-rua";
+        this.cor = "Malhado";
+        this.idade = 3;
 
     }
 }
+
+class Javali extends Animal {
+    public Javali() {
+        this.onomatopeia = "*BARULHO-DE-JAVALI*";
+        this.raca = "PRAGA";
+        this.cor = "Preto";
+        this.idade = 12;
+
+    }
+}
+
